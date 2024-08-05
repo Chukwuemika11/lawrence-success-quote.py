cd7daef6355714f50339a6c1b5411b3a260ef260
Chukwuemika11 <izunduchukwuemika9@gmail.com>
2024-08-05 22:29:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print("hello world!")
+
+print("work hard every waking hour by 'Elon Musk'")
+print("the road to success is very narrow but there are many roads to success, so which ever part you choose that leads to success is still going to be narrow(obstacles are bound to happen so be ready for all of it) 'Izundu Chukwuemika Ignatius Lawrence' ")
